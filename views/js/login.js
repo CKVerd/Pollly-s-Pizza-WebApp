111495181107ae9f72768f0407ba939d4a9225cc
Cyverd <verdad.cyrilken@gmail.com>
2020-11-13 13:50:46 +0800

    added login page with primitive js

---
+++ views/js/login.js
@@ -0,0 +1,25 @@
+var objPeople = [
+	{ 
+		username: "admin",
+		password: "admin"
+	},
+	{
+		username: "polly",
+		password: "pizza"
+	},
+]
+
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+			window.location.href = "index.html";
+			// stop the function if this is found to be true
+			return false
+		}
+	}
+	console.log("incorrect username or password")
+}
