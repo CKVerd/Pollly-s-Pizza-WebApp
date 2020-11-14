5ce6b7a18afacfaf9bd7c9573dfb9f8ee1e30efd
Cyverd <verdad.cyrilken@gmail.com>
2020-11-14 20:33:45 +0800

    added pop up

--- views/js/main.js
+++ views/js/main.js
@@ -1,3 +1,15 @@
 function logout() {
     window.location.href = "login.html";
 }
+
+function showPopup() {
+    var popup = document.querySelector('.popup');
+    popup.classList.remove('make-invisible')
+    popup.classList.add('make-visible')
+}
+
+function closePopup() {
+    var popup = document.querySelector('.popup');
+    popup.classList.remove('make-visible')
+    popup.classList.add('make-invisible')
+}
