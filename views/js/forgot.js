13abcf5525c046389491c0173a2ee793be066f41
Cyverd <verdad.cyrilken@gmail.com>
2020-11-24 23:24:56 +0800

    fixed forgot-pass

--- views/js/forgot.js
+++ views/js/forgot.js
@@ -5,8 +5,10 @@ function next() {
     row2.classList.add('appear')
 }
 
-function closePopup() {
-    var popup = document.querySelector('.popup');
-    popup.classList.remove('make-visible')
-    popup.classList.add('make-invisible')
+function next2() {
+    var row2 = document.querySelector('.row-body-2');
+    var row3 = document.querySelector('.row-body-3');
+    row2.classList.remove('appear')
+    row2.classList.add('remove')
+    row3.classList.add('appear')
 }
