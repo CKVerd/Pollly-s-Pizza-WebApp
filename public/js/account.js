970ed94854d16ff8df6fb538aacb6e5612f79838
Cyverd <verdad.cyrilken@gmail.com>
2021-02-11 00:33:34 +0800

    added functionality to the add account button
    add account form added

--- public/js/account.js
+++ public/js/account.js
@@ -1,38 +1,51 @@
 function showChangeUsern() {
     var popup = document.querySelector('.change-usern-popup');
-    popup.classList.remove('make-invisible')
-    popup.classList.add('make-visible')
+    popup.classList.remove('make-invisible');
+    popup.classList.add('make-visible');
     var content = document.querySelector('.change-usern-1');
     var content2 = document.querySelector('.change-usern-2');
-    content2.classList.remove('appear')
-    content2.classList.add('disappear')
-    content.classList.remove('remove')
-    content.classList.add('appear')
+    content2.classList.remove('appear');
+    content2.classList.add('disappear');
+    content.classList.remove('remove');
+    content.classList.add('appear');
 }
 
 function closeUsernPopup() {
     var popup = document.querySelector('.change-usern-popup');
-    popup.classList.remove('make-visible')
-    popup.classList.add('make-invisible')
+    popup.classList.remove('make-visible');
+    popup.classList.add('make-invisible');
 }
 
 function usernNext1() {
     var content = document.querySelector('.change-usern-1');
     var content2 = document.querySelector('.change-usern-2');
-    content.classList.remove('appear')
-    content.classList.add('remove')
-    content2.classList.remove('remove')
-    content2.classList.add('appear')
+    content.classList.remove('appear');
+    content.classList.add('remove');
+    content2.classList.remove('remove');
+    content2.classList.add('appear');
 }
 
 function showChangePass() {
     var popup = document.querySelector('.change-pass-popup');
-    popup.classList.remove('make-invisible')
-    popup.classList.add('make-visible')
+    popup.classList.remove('make-invisible');
+    popup.classList.add('make-visible');
 }
 
 function closeChangePassPopup() {
     var popup = document.querySelector('.change-pass-popup');
-    popup.classList.remove('make-visible')
-    popup.classList.add('make-invisible')
+    popup.classList.remove('make-visible');
+    popup.classList.add('make-invisible');
+}
+
+function showAddAccountPopup() {
+    var popup = document.querySelector (".add-account-popup");
+    var popup = document.querySelector (".add-account-popup");
+    popup.classList.remove('make-invisible');
+    popup.classList.add('make-visible');
+}
+
+function closeAddAccountPopup() {
+    var popup = document.querySelector (".add-account-popup");
+    popup.classList.remove('make-visible');
+    popup.classList.add('make-invisible');
 }
