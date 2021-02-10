7b3e23bfaa322c11443299bed2a0f5729f8ef55a
Cyverd <verdad.cyrilken@gmail.com>
2021-02-11 02:35:29 +0800

    added inventory js

---
+++ public/js/inventory.js
@@ -0,0 +1,14 @@
+function showAddPopup() {
+    var popup = document.querySelector('.add-stock-popup');
+    var popup2 = document.querySelector('.add-stock-popup-form');
+    popup.classList.remove('make-invisible');
+    popup.classList.add('make-visible');
+    popup2.classList.remove ('disappear')
+    popup2.classList.add ('appear')
+}
+
+function closeChangePassPopup() {
+    var popup = document.querySelector('.change-pass-popup');
+    popup.classList.remove('make-visible');
+    popup.classList.add('make-invisible');
+}
