965c937816d4106b1602eccc1922f7212416e219
Cyverd <verdad.cyrilken@gmail.com>
2021-02-18 17:29:06 +0800

    Added add product button and fixed layout for 1.25

--- public/js/sales.js
+++ public/js/sales.js
@@ -17,3 +17,18 @@ function removeClick() {
     item.innerHTML = out;
     console.log(out);
 }
+
+function showAddProductPopup() {
+    var popup = document.querySelector('.add-product-popup');
+    var popup2 = document.querySelector('.add-product-popup-form');
+    popup.classList.remove('make-invisible');
+    popup.classList.add('make-visible');
+    popup2.classList.remove ('disappear')
+    popup2.classList.add ('appear')
+}
+
+function closeAddProductPopup() {
+    var popup = document.querySelector('.add-product-popup');
+    popup.classList.remove('make-visible');
+    popup.classList.add('make-invisible');
+}
