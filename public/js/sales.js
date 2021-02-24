baf3a10599f034f6173341f81d42f65d96624011
Cyverd <verdad.cyrilken@gmail.com>
2021-02-24 22:26:04 +0800

    added delete sale confirmation form

--- public/js/sales.js
+++ public/js/sales.js
@@ -32,3 +32,19 @@ function closeAddProductPopup() {
     popup.classList.remove('make-visible');
     popup.classList.add('make-invisible');
 }
+
+function showConfirmDeletePopup() {
+    var popup = document.querySelector('.confirm-delete-popup');
+    var popup2 = document.querySelector('.confirm-delete-popup-form');
+    popup.classList.remove('make-invisible');
+    popup.classList.add('make-visible');
+    popup2.classList.remove ('disappear')
+    popup2.classList.add ('appear')
+}
+
+function closeConfirmDeletePopup() {
+    var popup = document.querySelector('.confirm-delete-popup');
+    popup.classList.remove('make-visible');
+    popup.classList.add('make-invisible');
+}
+
