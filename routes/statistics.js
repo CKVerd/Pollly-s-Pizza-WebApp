7960c51a3fe5d372663ff87d6dc263f6a758631f
Marrrrrrccc <60645901+Marrrrrrccc@users.noreply.github.com>
2021-03-19 09:41:38 +0800

    aaa

--- routes/statistics.js
+++ routes/statistics.js
@@ -46,6 +46,8 @@ router.get("/statistics",middleware.auth,(req,res)=>{
                 totalSum.push(bar[i].totalSum);
 
               }
+              console.log(DT)
+              console.log(totalSum)
            
              
                   if(err){
